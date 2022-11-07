6a809d411567df0c0696edb5aac400649ac33a46
LunasHere <lunaashere@gmail.com>
2022-11-07 12:56:47 -0600

    Implemented Luckperms

--- src/main/java/com/lunashere/crosschat/crosschat/Main.java
+++ src/main/java/com/lunashere/crosschat/crosschat/Main.java
@@ -1,5 +1,8 @@
 package com.lunashere.crosschat.crosschat;
 
+import net.luckperms.api.LuckPerms;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
@@ -12,10 +15,17 @@ public final class Main extends JavaPlugin {
         getServer().getMessenger().registerIncomingPluginChannel(this, "BungeeCord", new ChannelListener());
         // Register the player chat listener
         getServer().getPluginManager().registerEvents(new PlayerChat(), this);
+        // Register Luckperms API
+        RegisteredServiceProvider<LuckPerms> provider = Bukkit.getServicesManager().getRegistration(LuckPerms.class);
+        if (provider != null) {
+            LuckPerms api = provider.getProvider();
+
+        }
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
     }
+
 }
