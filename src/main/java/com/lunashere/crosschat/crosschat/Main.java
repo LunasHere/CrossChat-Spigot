9872c841ab44122237fec047470ac86f19e60284
LunasHere <lunaashere@gmail.com>
2022-11-07 13:21:45 -0600

    Removed unneeded import

--- src/main/java/com/lunashere/crosschat/crosschat/Main.java
+++ src/main/java/com/lunashere/crosschat/crosschat/Main.java
@@ -3,7 +3,6 @@ package com.lunashere.crosschat.crosschat;
 import com.google.common.io.ByteArrayDataOutput;
 import com.google.common.io.ByteStreams;
 import net.luckperms.api.LuckPerms;
-import net.luckperms.api.LuckPermsProvider;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.RegisteredServiceProvider;
